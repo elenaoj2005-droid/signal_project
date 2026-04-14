fb40e534a6b1653408ed6cd0550132988ed18b9c
elenaortega <elenaoj2005@gmail.com>
2026-04-14 23:51:45 +0200

    Add compiled JAR to bin directory and fix compilation errors

--- src/main/java/com/data_management/Patient.java
+++ src/main/java/com/data_management/Patient.java
@@ -52,6 +52,7 @@ public class Patient {
      *         range
      */
     public List<PatientRecord> getRecords(long startTime, long endTime) {
-        // TODO Implement and test this method
+        // TODO Implement and test this method -- return nothing for now". It's a temporary placeholder so the build doesn't crash
+        return null;
     }
 }
